d6087c02558238550ebb8c458f3a63a1d3bf59c8
jlavrin <julian.lavrin2@wakacje.pl>
2023-03-24 21:12:31 +0100

    #15 NESTJS implementation

--- src/handlers/errorHandler.ts
+++
@@ -1,8 +0,0 @@
-import { ErrorRequestHandler } from 'express'
-
-const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
-  console.error(error)
-  res.status(500).send('Internal server error')
-}
-
-export default errorHandler
